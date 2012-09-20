9a74d81248eeb49fbc009aff0952ae63bbbb0627
Janis Miezitis <janjiss@gmail.com>
2012-09-20 18:08:14 +0300

    Added new version and fixed rails bug with html_safe

--- lib/conred/helpers.rb
+++ lib/conred/helpers.rb
@@ -5,20 +5,20 @@ module Conred
   module Helpers
     
     extend self
-
-    @action_view = ActionView::Base.new
+    mattr_accessor :action_view
+    self.action_view = ActionView::Base.new
 
     def sanitize_and_trim(text, word_count = nil, omission = '...')
-      text = @action_view.strip_tags(text)
+      text = action_view.strip_tags(text)
       if word_count
-        @action_view.truncate(text, :length => word_count, :omission => omission)
+        action_view.truncate(text, :length => word_count, :omission => omission)
       else
         text
       end
     end
 
     def sanitize_body(text)
-      text = @action_view.sanitize(text, :tags => %w(p a strong ul ol li blockquote strike u em), :attributes => %w(href))
+      text = action_view.sanitize(text, :tags => %w(p a strong ul ol li blockquote strike u em), :attributes => %w(href))
       text.html_safe
     end
 
