d47a178696eb4e9037720814aa17b73298848534
Manmeet Saluja <manmeet.saluja94@gmail.com>
2014-12-14 19:30:32 -0800

    view count feature for youtube and vimeo implemented

--- lib/conred/video.rb
+++ lib/conred/video.rb
@@ -38,11 +38,34 @@ module Conred
     end
 
     def exist?
-      response = Net::HTTP.get_response(URI("http:#{api_uri}"))
+      response = get_api_response
       response.is_a?(Net::HTTPSuccess)
+    end
+
+    def viewCount
+      if self.exist?
+        body = get_api_response.body
+
+        if self.youtube_video?
+          body_json = Hash.from_xml(response.body).to_json
+          body_hash = JSON.parse(body_json)
+          body_hash["entry"]["statistics"]["viewCount"]
+        end
+
+        if self.vimeo_video?
+          body_hash = JSON.parse(body)
+          body_hash['stats_number_of_plays']
+        end
+      end
     end
 
     attr_reader :height, :width, :video_id, :error_message
 
+  end
+
+  private 
+
+  def get_api_response
+    Net::HTTP.get_response(URI("http:#{api_uri}"))
   end
 end
